bf865f29e3e3f6949bbf662a343aaeb11be879c1
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-06-09 15:25:46 +0100

    init

---
+++ src/main/java/com/sourcing/sourcing_reporting/SourcingReportingApplication.java
@@ -0,0 +1,13 @@
+package com.sourcing.sourcing_reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcingReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcingReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sourcing/sourcing_reporting/SourcingReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcing.sourcing_reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcingReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
